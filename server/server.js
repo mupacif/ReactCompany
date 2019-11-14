54b68cd6d3103c3053f449689456ab129a6c4c30
mupacif <mupacif@gmail.com>
2019-11-14 22:49:49 +0100

    ADD mysql config

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const graphqlHttp  = require('express-graphql');
-const graphqlSchema = require('./graphql/schema')
-const graphqlResolver = require('./graphql/resolver')
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolver');
+var mysql = require('mysql');
+const cors = require('cors');
+const keys = require('./keys');
 
 var app = express();
+app.use(cors()); //cross origins
+app.use(bodyParser.json());
+
 const PORT = 1337;
+
+//Mysql 
+app.use((req, res, next) => {
+    req.mysqlDb = mysql.createConnection({
+      host     : keys.host,
+      user     : keys.user,
+      password : keys.password,
+      database : keys.database
+    });
+    req.mysqlDb.connect();
+    next();
+  });
+
+
 
 
 // Serve the Parse API on the /parse URL prefix
