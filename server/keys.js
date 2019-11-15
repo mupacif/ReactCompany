f2242e40dc0548f501225f7b78ead07169b786d1
mupacif <mupacif@gmail.com>
2019-11-15 15:12:05 +0100

    ADD docker files #2

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     host     : process.env.MYSQL_HOST,
     user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DB
-}
+    password : process.env.MYSQL_ROOT_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+}   
