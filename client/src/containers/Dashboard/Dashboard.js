b56edd0d0237ca5e9b461c8beac12f9ad825ca73
Bart Sevenois <bart.sevenois@EfficyLounge.local>
2019-11-15 16:36:21 +0100

    routing client

---
+++ client/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+/* Packages */
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return(
+      <div className="dashboard">
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
