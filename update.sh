5d4eb051e3debe1dd36ef0f415636125360b102c
root <root@vps685664.ovh.net>
2020-02-03 00:15:18 +0100

    final deploy

--- update.sh
+++ update.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 cd ~/ReactCompany
-git fetch origin master
-cd ~/server
-npm install #npm module that doesnt want to install :/
-cd ..
 docker stack deploy -c Docker-compose.yml test
