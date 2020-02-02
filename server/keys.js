66a65ff0a1aee2d3d3816606a6bd04ec8d4d2166
Billionmoneybaby <reactcomp@vps685664.ovh.net>
2020-02-02 19:46:50 +0100

    fix server keys

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host     : process.env.MYSQL_HOST,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_ROOT_PASSWORD,
-    database : process.env.MYSQL_HOST
+    host     : process.env.MYSQL_HOST || 'mysql',
+    user     : process.env.MYSQL_USER || 'test',
+    password : process.env.MYSQL_ROOT_PASSWORD || 'test',
+    database : process.env.MYSQL_HOST || 'mysql'
 }   
