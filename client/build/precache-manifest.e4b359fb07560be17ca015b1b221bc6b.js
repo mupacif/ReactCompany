39b6c3ab78bd23b1947418b580d79735a7aecfc6
Pacifique <mupacif@gmail.com>
2021-03-12 00:59:43 +0100

    build to the moon

---
+++ client/build/precache-manifest.e4b359fb07560be17ca015b1b221bc6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c302f03afa60c3199ed2f1be4ccec609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c956c035bd9870a6b87d",
+    "url": "/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "0be69a4136d35b337ccd",
+    "url": "/static/js/2.c14c16df.chunk.js"
+  },
+  {
+    "revision": "c956c035bd9870a6b87d",
+    "url": "/static/js/main.632fdc40.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
