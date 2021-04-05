3f2907744a1b42bc7a440bfeaf30446bba1481c0
Pacifique <mupacif@gmail.com>
2021-04-06 00:13:14 +0200

    update

---
+++ client/build/precache-manifest.376fec201627715d4ea7806b39335746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eab388c549032fd5bdfe2d489766ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f1ac5adaadf4df60de",
+    "url": "/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "6fc05e1991b6ea36ee0c",
+    "url": "/static/js/2.4f7572a4.chunk.js"
+  },
+  {
+    "revision": "80f1ac5adaadf4df60de",
+    "url": "/static/js/main.a4da9a7a.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
